5486b164544e96026c52154b15c49233914278da
DRed <dima.redkin.1999@gmail.com>
2020-04-01 23:31:01 +0500

    add hibernate connection url

--- src/main/java/dcrm/service/ServiceApplication.java
+++ src/main/java/dcrm/service/ServiceApplication.java
@@ -1,5 +1,4 @@
 package dcrm.service;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
     }
-
 }
