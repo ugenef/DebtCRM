9f56e15a6056d94c21087fedec26978f626a8f7b
DRed <dima.redkin.1999@gmail.com>
2020-04-06 15:49:53 +0500

    Переделал отношения ManyToMany на помощью аннотации (до этого были классы типа StudentSubjectEntity)

--- src/main/java/models/SubjectEntity.java
+++ src/main/java/models/SubjectEntity.java
@@ -2,31 +2,40 @@ package models;
 
 import javax.persistence.*;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "subject", schema = "public", catalog = "dddj0qvpae0k6u")
 public class SubjectEntity {
-    private int subjectId;
-    private String subjectName;
 
     @Id
     @Column(name = "subject_id")
-    public int getSubjectId() {
-        return subjectId;
-    }
-
-    public void setSubjectId(int subjectId) {
-        this.subjectId = subjectId;
-    }
+    private int id;
 
     @Basic
     @Column(name = "subject_name")
-    public String getSubjectName() {
-        return subjectName;
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "subjects")
+    private Set<StudentEntity> students;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "subjects")
+    private Set<TeacherEntity> teachers;
+
+    public int getId() {
+        return id;
     }
 
-    public void setSubjectName(String subjectName) {
-        this.subjectName = subjectName;
+    public void setId(int subjectId) {
+        this.id = subjectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String subjectName) {
+        this.name = subjectName;
     }
 
     @Override
@@ -34,12 +43,20 @@ public class SubjectEntity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SubjectEntity that = (SubjectEntity) o;
-        return subjectId == that.subjectId &&
-                Objects.equals(subjectName, that.subjectName);
+        return id == that.id &&
+                Objects.equals(name, that.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(subjectId, subjectName);
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "SubjectEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/main/java/models/TeacherEntity.java
+++ src/main/java/models/TeacherEntity.java
@@ -2,27 +2,44 @@ package models;
 
 import javax.persistence.*;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "teacher", schema = "public", catalog = "dddj0qvpae0k6u")
 public class TeacherEntity {
-    private int teacherId;
-    private String firstName;
-    private String middleName;
-    private String lastName;
 
     @Id
     @Column(name = "teacher_id")
-    public int getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(int teacherId) {
-        this.teacherId = teacherId;
-    }
+    private int id;
 
     @Basic
     @Column(name = "first_name")
+    private String firstName;
+
+    @Basic
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Basic
+    @Column(name = "last_name")
+    private String lastName;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "teacher_subject",
+            joinColumns = @JoinColumn(name = "teacher_id"),
+            inverseJoinColumns = @JoinColumn(name = "subject_id"))
+    private Set<SubjectEntity> subjects;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int teacherId) {
+        this.id = teacherId;
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -31,8 +48,6 @@ public class TeacherEntity {
         this.firstName = firstName;
     }
 
-    @Basic
-    @Column(name = "middle_name")
     public String getMiddleName() {
         return middleName;
     }
@@ -41,8 +56,6 @@ public class TeacherEntity {
         this.middleName = middleName;
     }
 
-    @Basic
-    @Column(name = "last_name")
     public String getLastName() {
         return lastName;
     }
@@ -56,7 +69,7 @@ public class TeacherEntity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TeacherEntity that = (TeacherEntity) o;
-        return teacherId == that.teacherId &&
+        return id == that.id &&
                 Objects.equals(firstName, that.firstName) &&
                 Objects.equals(middleName, that.middleName) &&
                 Objects.equals(lastName, that.lastName);
@@ -64,6 +77,16 @@ public class TeacherEntity {
 
     @Override
     public int hashCode() {
-        return Objects.hash(teacherId, firstName, middleName, lastName);
+        return Objects.hash(id, firstName, middleName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "TeacherEntity{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
